5233284afcf400c282026eaae405b512b40615b4
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2024-05-09 21:49:04 -0300

    09/05/24

--- src/main/java/br/com/muriloff/lojaLivro/Controller/LivrosController.java
+++ src/main/java/br/com/muriloff/lojaLivro/Controller/LivrosController.java
@@ -10,7 +10,7 @@ import  br.com.muriloff.lojaLivro.Repository.LivrosRepository;
 import  br.com.muriloff.lojaLivro.Model.Livro;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
